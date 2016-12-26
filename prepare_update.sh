a0ec150b558327f748cdd79186650f295a6ae86c
thaliondrambor <thaliondrambor@users.noreply.github.com>
2016-12-26 19:57:09 +0100

    Create prepare_update.sh

---
+++ prepare_update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash   
+rm controls_YeeLight.txt
+find ./FHEM -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f; 
+  do
+   echo "DEL ${f}" >> controls_YeeLight.txt
+   out="UPD "$(stat -c %y  $f | cut -d. -f1 | awk '{printf "%s_%s",$1,$2}')" "$(stat -c %s $f)" ${f}"
+   echo ${out//.\//} >> controls_YeeLight.txt
+done
+
+# CHANGED file
+echo "YeeLight and more last change:" > CHANGED
+echo $(date +"%Y-%m-%d") >> CHANGED
+echo " - $(git log -1 --pretty=%B)" >> CHANGED
