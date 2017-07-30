2f779fe0820ddeda27a00bc30d0605a1d8691878
thaliondrambor <thaliondrambor@users.noreply.github.com>
2017-07-30 15:51:23 +0200

    fixed bug not able to set keepAlive to 0

--- 32_YeeLightBridge.pm
+++ 32_YeeLightBridge.pm
@@ -1,10 +1,12 @@
 ##############################################
-# $Id: 32_YeeLightBridge.pm 2016-12-27 thaliondrambor $
+# $Id: 32_YeeLightBridge.pm 2017-07-30 thaliondrambor $
 #
 # versions
 # 00 start
 # 01 added timeout, keepAlive
 # 02 added Log at ReadFn
+# 03 search finally works, added attribut userScene[0-9]
+# 04 fixed bug, unable to set keepAlive attribut to 0
 #
 # verbose level
 # 0: quit
@@ -23,8 +25,9 @@ use IO::Socket::Multicast;
 use IO::Socket::INET;
 
 #----------------------------Constants------------------------------------------
-use constant GROUP => '239.255.255.250';	#Multicast Group
-use constant MPORT => 1982;					#Multicast Port
+use constant GROUP => '239.255.255.250';	# Multicast Group
+use constant MPORT => 1982;					# Multicast Port
+use constant LPORT => 13254;				# Local Port
 
 sub
 YeeLightBridge_Initialize
@@ -44,6 +47,8 @@ YeeLightBridge_Initialize
 		."updateIP:0,1 "
 		."timeout "
 		."keepAlive "
+		."userScene[0-9] "
+		."searchtime "
 		."$readingFnAttributes";
 		
 	# Comm with Devices
@@ -67,12 +72,11 @@ YeeLightBridge_Define
 	$hash->{NAME} = $name;
 	$hash->{MULTICAST_GROUP}	= GROUP;
 	$hash->{MULTICAST_PORT}		= MPORT;
-	$hash->{DeviceName}			= GROUP.':'.MPORT;
+	$hash->{DeviceName} 		= GROUP.":".MPORT;
 	
 	$attr{$name}{room} = "YeeLight" if(!defined( $attr{$name}{room}));
 	
 	YeeLightBridgeMulticast_Close($hash);
-	YeeLightBridgeMulticast_Create($hash);
 	YeeLightBridgeMulticast_Send($hash);
 	
 	$modules{YeeLightBridge}{defptr} = $hash;
@@ -99,11 +103,10 @@ YeeLightBridge_Set
 	my ($hash, $name, $cmd, @val) = @_;
     
 	my $list = "";
-	$list .= "search ";
+	$list .= "search:noArg ";
 	
 	if (lc $cmd eq 'search')
 	{
-		#return "$name: search command doesn't work yet. Sorry :-(";
 		return YeeLightBridgeMulticast_Send($hash);
 	}
 	
@@ -131,7 +134,15 @@ YeeLightBridge_Attr
 		}
 		elsif ($attrName eq "keepAlive")
 		{
-			return "Invalid parameter for $attrName. $attrName must be numeric and at least 60 or 0." if ($attrVal !~ /^\d?.?\d+$/) || (($attrVal < 60) && ($attrVal == 0));
+			return "Invalid parameter for $attrName. $attrName must be numeric and at least 60 or 0." if ($attrVal !~ /^\d?.?\d+$/) || (($attrVal < 60) && ($attrVal != 0));
+		}
+		elsif ($attrName =~ /userScene[0-9]/)
+		{
+		
+		}
+		elsif ($attrName eq "searchtime")
+		{
+			return "Invalid parameter for $attrName. $attrName must be numeric and greater than 0." if ($attrVal !~ /^\d?.?\d+$/) || ($attrVal < 1);
 		}
 	}
 	
@@ -143,11 +154,11 @@ YeeLightBridge_Read
 {
 	my ($hash) = @_;
 	my $name = $hash->{NAME};
-	my $mcast = $hash->{helper}->{mcast_sock};
+	my $sock = $hash->{helper}->{sock};
 	
 	my $buf;
 	
-	$mcast->recv($buf,1024);
+	$sock->recv($buf,1024);
 	Log3 $name, 4, "received multicast message on 239.255.255.250:1982:\r\n$buf";
 	Dispatch($hash,$buf,0);
 }
@@ -167,16 +178,21 @@ YeeLightBridgeMulticast_Create
 	my ($hash) = @_;
 	my $name = $hash->{NAME};
 	
+	YeeLightBridgeMulticast_Close($hash);
+	
 	my $mcast = IO::Socket::Multicast->new(Proto		=> 'udp',
 											LocalPort	=> MPORT,
 											ReuseAddr	=> 1)
 											or return "Can't bind mcast: $@\n";
-											
-	$mcast->mcast_add(GROUP) or return "Couldn't set group: $!\n";
 	
-	$hash->{helper}->{mcast_sock}	= $mcast;
-	$hash->{FD}						= $mcast->fileno();
-	$selectlist{$hash->{NAME}}		= $hash;
+	Log3 $name, 4, "$name: opened socket for multicast messages";
+	
+	$mcast->mcast_add(GROUP) or return "Couldn't set group: $!\n";
+	$hash->{STATE} = "CONNECTED";
+	
+	$hash->{helper}->{sock}		= $mcast;
+	$hash->{FD}					= $mcast->fileno();
+	$selectlist{$name}			= $hash;
 	
 	return undef;
 }
@@ -188,12 +204,26 @@ YeeLightBridgeMulticast_Send
 	my $name = $hash->{NAME};
 	my $dev = $hash->{DeviceName};
 	
-	my $sock = IO::Socket::Multicast->new(Proto=>'udp',PeerAddr=>$dev, ReuseAddr=>1);
+	YeeLightBridgeMulticast_Close($hash);
 	
+	my $sock = IO::Socket::Multicast->new() or return "Can't bind socket: $@\n";
+	$sock->mcast_ttl(3);
+	
+	Log3 $name, 4, "$name: opened socket for search message";
+	$hash->{STATE} = "SEARCHING";
+	
+	$hash->{helper}->{sock}		= $sock;
+	$hash->{FD}					= $sock->fileno();
+	$selectlist{$name}			= $hash;
+		
 	my $send = qq(M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1982\r\nMAN: "ssdp:discover"\r\nST: wifi_bulb\r\n);
-	$sock->mcast_send($send, $dev) or return "Couldn't send message: $!";
+	$sock->mcast_send($send,$dev) or return "Couldn't send message: $!";
 	Log3 $name, 4, "$name: Search with $send";
-	$sock->close();
+	
+	my $searchTime	= 3;
+	$searchTime		= $attr{$name}{searchtime} if defined($attr{$name}{searchtime});
+	
+	InternalTimer(gettimeofday() + $searchTime,"YeeLightBridgeMulticast_Create",$hash);
 	
 	return undef;
 }
@@ -204,13 +234,16 @@ YeeLightBridgeMulticast_Close
 	my ($hash) = @_;
 	my $name = $hash->{NAME};
 	
-	if (defined($hash->{helper}->{mcast_sock}))
+	if (defined($hash->{helper}->{sock}))
 	{
-		$hash->{helper}->{mcast_sock}->close();
-		delete($hash->{helper}->{mcast_sock});
+		$hash->{helper}->{sock}->close();
+		delete($hash->{helper}->{sock});
 		delete($selectlist{$name});
-		delete($hash->{helper}->{mcast_FD});
+		delete($hash->{FD});
+		Log3 $name, 4, "$name: closing socket.";
 	}
+	
+	$hash->{STATE} = "DISCONNECTED";
 	
 	return undef;
 }
