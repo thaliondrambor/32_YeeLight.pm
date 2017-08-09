2b1a4593fa91d58a1e007ccb32399d49c5f22d68
thaliondrambor <thaliondrambor@users.noreply.github.com>
2017-08-09 16:03:26 +0200

    Update prepare_update.sh

--- prepare_update.sh
+++ prepare_update.sh
@@ -2,7 +2,6 @@
 rm controls_YeeLight.txt
 find ./FHEM -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f; 
   do
-   echo "DEL ${f}" >> controls_YeeLight.txt
    out="UPD "$(stat -c %y  $f | cut -d. -f1 | awk '{printf "%s_%s",$1,$2}')" "$(stat -c %s $f)" ${f}"
    echo ${out//.\//} >> controls_YeeLight.txt
 done
